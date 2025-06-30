54edf4158da9e73f96ff671c6c25e8d93c990d7f
RosanKarthik <rosankarthik@outlook.com>
2025-06-30 12:12:08 +0530

    init

---
+++ Inc/hcsr_04.h
@@ -0,0 +1,70 @@
+/*
+ * hcsr_04.h
+ *
+ *  Created on: Jun 30, 2025
+ *      Author: HAI
+ */
+
+#ifndef INC_HCSR_04_H_
+#define INC_HCSR_04_H_
+
+#define STM32XXXX_HAL_HEADER_FILE "stm32f4xx_hal.h" //example "stm32g4xx_hal.h"
+#define STM32XXXX_HAL_TIM_HEADER_FILE "stm32f4xx_hal_tim.h" //example "stm32g4xx_hal_tim.h"
+
+#include STM32XXXX_HAL_HEADER_FILE
+#include STM32XXXX_HAL_TIM_HEADER_FILE
+
+//Status Of The Sensor For Error Handling
+typedef enum {
+	HCSR04_OK = 0,
+	HCSR04_TIMEOUT = 1
+} HCSR04_StatusTypeDef;
+
+//Structure To Store Configuration Data For The Sensor
+typedef struct{
+	float distance;
+	GPIO_TypeDef * _EchoGPIOx;
+	GPIO_TypeDef * _TrigGPIOx;
+	uint16_t _EchoPin;
+	uint16_t _TrigPin;
+	TIM_HandleTypeDef * _TIM;
+}HCSR04_InitTypeDef;
+
+/**
+  * @brief  Intitalizes the HCSR04 Driver.
+  * @param	HCSR instance of a HCSR04 driver.
+  * @param  _EchoGPIOx where x can be (A..G) to select the GPIO peripheral for STM32xxxx family.
+  * @param  _EchoPin specifies the pin Echo is connected.
+  * @param  _TrigGPIOx where x can be (A..G) to select the GPIO peripheral for STM32xxxx family.
+  * @param  _TrigPin specifies the pin Trig is connected.
+  * @param 	TIM handler for timer with frequency to count 1us.
+  * @retval None
+  */
+void HCSR04_Init(
+		HCSR04_InitTypeDef * HCSR,
+		GPIO_TypeDef * _EchoGPIOx,
+		GPIO_TypeDef * _TrigGPIOx,
+		uint16_t _EchoPin,
+		uint16_t _TrigPin,
+		TIM_HandleTypeDef * _TIM
+	)
+
+/**
+  * @brief  DeIntitalizes the HCSR04 Driver.
+  * @param	HCSR instance of a HCSR04 driver.
+  * @retval None
+  */
+void HCSR04_DeInit(HCSR04_InitTypeDef *HCSR);
+
+/**
+  * @brief  Calculates the distances and stores it in *distance
+  * @param	HCSR instance of a HCSR04 driver.
+  * @param 	Distance variable to store the output.
+  * @retval Returns the status of the sensor.
+  */
+HCSR04_StatusTypeDef HCSR04_ReadDistance(
+	HCSR04_InitTypeDef *hcsr,
+	float *distance
+);
+
+#endif /* INC_HCSR_04_H_ */
---
+++ Src/hcsr_04.c
@@ -0,0 +1,62 @@
+/*
+ * hcsr_04.c
+ *
+ *  Created on: Jun 30, 2025
+ *      Author: HAI
+ */
+
+static void HCSR04_DelayUs(TIM_HandleTypeDef * tim, uint16_t us){
+	__HAL_TIM_SET_COUNTER(&tim,0);
+	while(__HAL_TIM_GET_COUNTER(&tim)<us){
+		__NOP();
+	}
+}
+
+void HCSR04_Init(
+		HCSR04_InitTypeDef * HCSR,
+		GPIO_TypeDef * _EchoGPIOx,
+		GPIO_TypeDef * _TrigGPIOx,
+		uint16_t _EchoPin,
+		uint16_t _TrigPin,
+		TIM_HandleTypeDef * _TIM
+	){
+	HCSR->distance=0;
+	HCSR->_EchoGPIOx=_EchoGPIOx;
+	HCSR->_TrigGPIOx=_TrigGPIOx;
+	HCSR->_EchoPin=_EchoPin;
+	HCSR->_TrigPin=_TrigPin;
+	HCSR->_TIM=_TIM;
+	HAL_TIM_Base_Start(HCSR->_TIM);
+}
+
+void HCSR04_DeInit(HCSR04_InitTypeDef * HCSR){
+	HAL_GPIO_DeInit(HCSR->_EchoGPIOx,HCSR->_EchoPIN);
+	HAL_GPIO_DeInit(HCSR->_TrigGPIOx,HCSR->_TrigPIN);
+	HAL_TIM_Base_Stop(HCSR->_TIM);
+}
+
+HCSR04_StatusTypeDef HCSR04_GET_DISTANCE(HCSR04_InitTypeDef * HCSR){
+	uint16_t t_taken=0;
+	uint16_t timeout;
+	HAL_GPIO_WritePin(HCSR->_TrigGPIOx,HCSR->_TrigPin,GPIO_PIN_RESET);
+	HCSR04_DelayUS(HCSR->_TIM,2);
+	HAL_GPIO_WritePin(HCSR->_TrigGPIOx,HCSR->_TrigPin,GPIO_PIN_SET);
+	HCSR04_DelayUS(HCSR->_TIM,10);
+	HAL_GPIO_WritePin(HCSR->_TrigGPIOx,HCSR->_TrigPin,GPIO_PIN_RESET);
+
+	timeout=1000;
+	while(!HAL_GPIO_ReadPin(HCSR->_EchoGPIOx,HCSR->_EchoPin)){
+		if(--timeout) return HCSR04_TIMEOUT;
+	}
+
+	timeout=1000;
+	while(HAL_GPIO_ReadPin(HCSR->_EchoGPIOx,HCSR->_EchoPin)){
+		if(--timeout) return HCSR04_TIMEOUT;
+		t_taken++;
+		HCSR04_DelayUS(HCSR->_TIM,1);
+	}
+	*distance=t_taken/58.0f;
+	HCSR->distance=*distance;
+	return HCSR04_OK;
+}
+
